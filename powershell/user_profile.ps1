ad339cd578a03580d78195f3f5c39458eec7ac6f
Frazix12 <irfanhossain683@gmail.com>
2023-06-24 21:09:49 +0600

    Stuff

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -2,30 +2,16 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 #Import's
-# Import-Module posh-git
-# Import-Module PSFzf
+Import-Module posh-git
 
 $omp_config = Join-Path $PSScriptRoot ".\irfan.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
-# PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-Set-PSReadLineOption -PredictionSource History
-
-# Fzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# Env
-$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
-
 #Function's
 function OpenVSC { code . }
-function GitClone { git clone @args }
-function CodeDirectory { Set-Location D:\CODE }
+function CodeDirectory { Set-Location E:\CODE }
 function Invoke-Tere() {
   $result = . C:\Users\irfan\.config\tree\tere.exe $args
   if ($result) {
@@ -35,14 +21,12 @@ function Invoke-Tere() {
 # Alias
 Set-Alias tere Invoke-Tere
 Set-Alias vc OpenVSC
-Set-Alias gc GitClone
 Set-Alias cdc CodeDirectory
 Set-Alias btw winfetch
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias vim nvim
 
 # Utilities
 function which ($command) {
